f96b2b532732bc499282fdee07ae0961cc7b824d
Benardo Peralta <chrisperalta1998@gmail.com>
2022-02-14 10:33:13 -0800

    done

--- index.js
+++ index.js
@@ -2,24 +2,27 @@
 
 /**Example Task : processFirstItem()
  * This example shows how you might go about solving the rest of the tasks
- * 
+ *
  * Use the higher order function processFirstItem below to do the following:
  *  1. Receive an array of strings in a parameter
  *  2. Receive a callback function that takes a string as its argument in a parameter
- *  3. Return the result of invoking the callback function and passing in the FIRST 
+ *  3. Return the result of invoking the callback function and passing in the FIRST
  *     element in the array as the argument
- * 
+ *
  * The following code is demonstrating a way of completing this task
  * It returns the string `foofoo`
-*/
+ */
 
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  return callback(stringList[0]);
 }
-console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
+console.log(
+  processFirstItem(["foo", "bar"], function (str) {
+    return str + str;
+  })
+);
 
 // ⭐️ Example Challenge END ⭐️
-
 
 ///// M V P ///////
 
@@ -39,8 +42,8 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
 function counterMaker() {
   let count = 0;
   return function counter() {
-   return count++;
-  }
+    return count++;
+  };
 }
 
 const counter1 = counterMaker();
@@ -52,7 +55,6 @@ function counter2() {
   return count++;
 }
 
-
 /* ⚾️⚾️⚾️ Task 2: inning() ⚾️⚾️⚾️
 Use the inning function below to do the following:
   1. Return a random whole number of points between 0 and 2 scored by one team in an inning
@@ -62,10 +64,9 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning() {
+  return Math.floor(Math.random() * 3);
 }
-
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
@@ -79,10 +80,17 @@ Use the finalScore function below to do the following:
   "Home": 11,
   "Away": 5
 }
-*/ 
+*/
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningCB, desiredInnings) {
+  let homeTeam = 0;
+  let awayTeam = 0;
+  for (let i = 0; i < desiredInnings; i++) {
+    homeTeam += inningCB();
+    awayTeam += inningCB();
+  }
+
+  return { Home: homeTeam, Away: awayTeam };
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
@@ -90,10 +98,11 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCB) {
+  let homeScore = inningCB();
+  let awayScore = inningCB();
+  return { Home: homeScore, Away: awayScore };
 }
-
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
@@ -136,17 +145,36 @@ Use the scoreboard function below to do the following:
 ]  
   */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(getInningScoreCB, inningCB, desiredInnings) {
+  let result = [];
+  let homeSum = 0;
+  let awaySum = 0;
+  for (let i = 0; i < desiredInnings; i++) {
+    let homeScore = inningCB();
+    let awayScore = inningCB();
+    homeSum += homeScore;
+    awaySum += awayScore;
+    result.push(`Inning ${i + 1}: Away ${awayScore} - Home ${homeScore}`);
+  }
+  if (homeSum === awaySum) {
+    result.push(
+      `This game will require extra innings: Away ${awaySum} - Home ${homeSum}`
+    );
+  } else {
+    result.push(`Final Score: Away ${awaySum} - Home ${homeSum}`);
+  }
+  return result;
 }
-
-
-
+console.log(scoreboard(getInningScore, inning, 5));
+console.log(scoreboard(getInningScore, inning, 3));
+console.log(scoreboard(getInningScore, inning, 8));
+console.log(scoreboard(getInningScore, inning, 9));
+console.log(scoreboard(getInningScore, inning, 10));
 
 /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
-function foo(){
-  console.log('its working');
-  return 'bar';
+function foo() {
+  console.log("its working");
+  return "bar";
 }
 foo();
 module.exports = {
@@ -158,4 +186,4 @@ module.exports = {
   finalScore,
   getInningScore,
   scoreboard,
-}
+};
